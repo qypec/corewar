30826dfdbca446c785eb8a285674ae740ea1ce01
ergottli <ergottli@student21.-school.ru>
2020-01-16 21:27:05 +0300

    make function to check magicheader

--- srcs/vm/parse_file.c
+++ srcs/vm/parse_file.c
@@ -12,22 +12,19 @@
 
 #include "../../incs/corewar.h"
 
-int check_magic(int fd, int player_k)
+int	check_magic(int fd, int player_k)
 {
-	int			*i;
-	char			buf[5];
-	unsigned char 	*temp;
+	unsigned char	buf[4];
 
-	if (read(fd, &buf, 4) < 1)
+	if (read(fd, &buf[3], 1) < 1)
 		return (0);
-	printf("%u\n", COREWAR_EXEC_MAGIC);
-	i = (int*)buf;
-//	i[0] = i[0] >> 8u;
-	printf("%u\n", i[0]);
-	printf("%lu\n", sizeof(COREWAR_EXEC_MAGIC));
-	printf("%lu\n", sizeof(i[0]));
-	if (i[0] != COREWAR_EXEC_MAGIC)
+	if (read(fd, &buf[2], 1) < 1)
 		return (0);
-
+	if (read(fd, &buf[1], 1) < 1)
+		return (0);
+	if (read(fd, &buf[0], 1) < 1)
+		return (0);
+	if (*(unsigned int*)buf != COREWAR_EXEC_MAGIC)
+		return (0);
 	return (1);
 }
