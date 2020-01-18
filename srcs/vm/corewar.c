ba9340337fde241a6ece708aca70374528d5dbae
vgerold <eu@concreteresult.ru>
2020-01-18 17:25:23 +0300

    .

--- srcs/vm/corewar.c
+++ srcs/vm/corewar.c
@@ -6,7 +6,7 @@
 /*   By: vgerold- <vgerold-@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 15:38:26 by vgerold-          #+#    #+#             */
-/*   Updated: 2020/01/16 18:46:49 by vgerold-         ###   ########.fr       */
+/*   Updated: 2020/01/18 17:23:18 by vgerold-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ int 	init_players_struct()
 	int i;
 
 	i = -1;
-	while (++i < MAX_PLAYERS)
+	while (++i <= MAX_PLAYERS)
 	{
 		vm.players[i].id = 0;
-		vm.players[i].name[PROG_NAME_LENGTH] = '\0';
-		vm.players[i].comment[COMMENT_LENGTH] = '\0';
+		vm.players[i].name = NULL;
+		vm.players[i].comment = NULL;
+		vm.players[i].code = NULL;
 		vm.players[i].code_size = 0;
 		vm.players[i].is_alive = 0;
 		vm.players[i].lives_all = 0;
@@ -40,7 +41,6 @@ int 	init_corewar()
 	vm.cycle_current = 0;
 	vm.cycles_to_die = CYCLE_TO_DIE;
 	vm.players_sum = 0;
-	ft_bzero(vm.arena, MEM_SIZE);
 	init_players_struct();
 	return (1);
 }
@@ -50,16 +50,10 @@ int 	main(int argc, char **argv)
 	if (argc <= 1)
 		return (print_usage(0, 1));
 	else
-		if (!init_corewar() || 	!check_args(argc, argv))
-			return (0);
-	if (!create_players(argv))
 	{
-		ft_printf("error with player code");
-		return (0);
+		if (!init_corewar())
+			return (0);
+		check_args(argc, argv);
 	}
-	printf("ok");
-	return (0);
-	if (!init_game())
-		return (0);
 	return (0);
 }
--- srcs/vm/create_players.c
+++ srcs/vm/create_players.c
@@ -6,7 +6,7 @@
 /*   By: ergottli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 17:47:07 by ergottli          #+#    #+#             */
-/*   Updated: 2020/01/16 17:47:08 by ergottli         ###   ########.fr       */
+/*   Updated: 2020/01/18 17:23:18 by vgerold-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,33 +35,12 @@ void	set_id(int player_k)
 
 int		load_player(char *player_filename, int player_k)
 {
-	int fd;
-
-	fd = 0;
 	if (vm.players[player_k].id == 0)
 		set_id(player_k);
-	if ((fd = open(player_filename, O_RDONLY)) < 1)
-		return (0);
-	if (!check_magic(fd, player_k))
-		return (0);
-	printf("1 - after magic\n");
-	if (!check_name(fd, player_k)) //check NULL
-		return (0);
-	printf("2 - after name\n");
-	if (!check_exec_size(fd, player_k))
-		return (0);
-	printf("3 - after exec size\n");
-	if (!check_comment(fd, player_k)) //check NULL
-		return (0);
-	printf("4 - after comment\n");
-	if (!check_code(fd, player_k))
-		return (0);
-	printf("5 - after code\n");
-	close(fd);
 	return (1);
 }
 
-int create_players(char **argv)
+int		create_players(char **argv)
 {
 	int i;
 	int k;
@@ -71,11 +50,8 @@ int create_players(char **argv)
 	while (argv[++i])
 	{
 		if (ft_strstr(argv[i], ".cor"))
-		{
 			if (!load_player(argv[i], k))
 				return (0);
-			++k;
-		}
 	}
 	return (1);
 }
