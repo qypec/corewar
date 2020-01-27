e5ab00dcca86c32733008721c1fa4f4a52b4f3cd
ergottli <ergottli@student21.-school.ru>
2020-01-27 05:51:01 +0300

    norminette check_args.c

--- srcs/vm/check_args.c
+++ srcs/vm/check_args.c
@@ -15,19 +15,24 @@
 int		print_usage(int code, int usage)
 {
 	if (code == -1)
-		ft_printf("%s\n", ANSI_B_RED"Only files with the extension .cor are accepted!"ANSI_RESET);
+		ft_printf("%s\n", ANSI_B_RED"Only files with "
+		"the extension .cor are accepted!"ANSI_RESET);
 	if (code == -2)
-		ft_printf("%s\n", ANSI_B_RED"The -n (1 - MAX_PLAYERS) flag overrides the player's sequence number, "
-						  "set before the player's file. "ANSI_RESET);
+		ft_printf("%s\n", ANSI_B_RED"The -n (1 - MAX_PLAYERS) flag "
+"overrides the player's sequence number, "
+"set before the player's file. "ANSI_RESET);
 	if (code == -3)
-		ft_printf("%s\n", ANSI_B_RED"The-d CYCLE flag prints the state of the arena on the specified cycle.\n"
-						  "The CYCLE value can only be a positive natural number\n"ANSI_RESET);
+		ft_printf("%s\n", ANSI_B_RED"The-d CYCLE flag prints "
+"the state of the arena on the specified cycle.\n"
+"The CYCLE value can only be a positive natural number\n"ANSI_RESET);
 	if (code == -4)
-		ft_printf("%s %d\n", ANSI_B_RED"Max number of players is:", MAX_PLAYERS, ANSI_RESET);
+		ft_printf("%s %d\n", ANSI_B_RED"Max number of players is:",
+				MAX_PLAYERS, ANSI_RESET);
 	if (code == -5)
 		ft_printf("%s\n", ANSI_B_RED"Duplicates in flags"ANSI_RESET);
 	if (code == -6)
-		ft_printf("%s\n", ANSI_B_RED"You pass FLAG as the LAST argument!"ANSI_RESET);
+		ft_printf("%s\n", ANSI_B_RED"You pass "
+"FLAG as the LAST argument!"ANSI_RESET);
 	if (code == -7)
 		ft_printf("%s\n", ANSI_B_RED"Flag value is not a number!"ANSI_RESET);
 	if (usage)
@@ -35,14 +40,19 @@ int		print_usage(int code, int usage)
 	return (0);
 }
 
-void    standart_usage(void)
+void	standart_usage(void)
 {
-	ft_putstr("Usage: ./corewar [-a (-dump|-d) <num> (-shed|-s) <num> -l <num>] [-v] [-n <num>] <champion.cor> <...>\n");
+	ft_putstr("Usage: ./corewar [-a (-dump|-d) <num> (-shed|-s) "
+"<num> -l <num>] [-v] [-n <num>] <champion.cor> <...>\n");
 	ft_putstr("    -a          : Print output from \"aff\" (Default is off)\n");
-	ft_putstr("    -dump <num> : Dump memory (32 octets per line) after <num> cycles and exit\n");
-	ft_putstr("    -d    <num> : Dump memory (64 octets per line) after <num> cycles and exit\n");
-	ft_putstr("    -show <num> : Run <num> cycles, dump memory (32 octets per line), pause and repeat\n");
-	ft_putstr("    -s    <num> : Run <num> cycles, dump memory (64 octets per line), pause and repeat\n");
+	ft_putstr("    -dump <num> : Dump memory "
+"(32 octets per line) after <num> cycles and exit\n");
+	ft_putstr("    -d    <num> : Dump memory "
+"(64 octets per line) after <num> cycles and exit\n");
+	ft_putstr("    -show <num> : Run <num> cycles, "
+"dump memory (32 octets per line), pause and repeat\n");
+	ft_putstr("    -s    <num> : Run <num> cycles, "
+"dump memory (64 octets per line), pause and repeat\n");
 	ft_putstr("    -l    <num> : Log levels\n");
 	ft_putstr("                   1  : Show lives\n");
 	ft_putstr("                   2  : Show cycles\n");
