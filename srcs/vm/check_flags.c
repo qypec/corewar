2fc0d3c6f654e155c038a016bcb853807f3b9cca
ergottli <ergottli@student21.-school.ru>
2020-01-27 05:54:34 +0300

    norminette check_flags.c

--- srcs/vm/check_flags.c
+++ srcs/vm/check_flags.c
@@ -1,37 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_flags.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ergottli <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/27 05:51:26 by ergottli          #+#    #+#             */
+/*   Updated: 2020/01/27 05:51:29 by ergottli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../incs/corewar.h"
 
-int check_n(int value)
+int	check_n(int value)
 {
-    if (value > MAX_PLAYERS || value <= 0)
-        return (print_usage(-2, 1));
-    else
-    {
-        if (is_set(value, MAX_PLAYERS))
-            return (print_usage(-5, 1));
-        vm.players_temp[vm.players_sum].id =
-                (!is_set(value, MAX_PLAYERS)) ? value : 0;
-    }
-    return (1);
+	if (value > MAX_PLAYERS || value <= 0)
+		return (print_usage(-2, 1));
+	else
+	{
+		if (is_set(value, MAX_PLAYERS))
+			return (print_usage(-5, 1));
+		vm.players_temp[vm.players_sum].id =
+				(!is_set(value, MAX_PLAYERS)) ? value : 0;
+	}
+	return (1);
 }
 
-int check_d(char **av, int value, int i)
+int	check_d(char **av, int value, int i)
 {
-    vm.dump |= (ft_strequ(av[i], "-dump")) ? 2u : 1u;
-    if(av[i + 1] <= 0)
-        return (print_usage(-3, 0));
-    else
-        vm.dump_cycle = value;
-    return (1);
+	vm.dump |= (ft_strequ(av[i], "-dump")) ? 2u : 1u;
+	if (av[i + 1] <= 0)
+		return (print_usage(-3, 0));
+	else
+		vm.dump_cycle = value;
+	return (1);
 }
 
-int check_s(char **av, int value, int i)
+int	check_s(char **av, int value, int i)
 {
 	vm.s |= (ft_strequ(av[i], "show")) ? 2u : 1u;
-	if(av[i + 1] <= 0)
+	if (av[i + 1] <= 0)
 	{
-		ft_printf(ANSI_B_RED"Negative or zero flag -s value! See usage:\n"ANSI_RESET);
-		ft_putstr("    -show <num> : Run <num> cycles, dump memory (32 octets per line), pause and repeat\n");
-		ft_putstr("    -s    <num> : Run <num> cycles, dump memory (64 octets per line), pause and repeat\n");
+		ft_printf(ANSI_B_RED"Negative or zero flag "
+			"-s value! See usage:\n"ANSI_RESET);
+		ft_putstr("    -show <num> : Run <num> cycles, "
+			"dump memory (32 octets per line), pause and repeat\n");
+		ft_putstr("    -s    <num> : Run <num> cycles, "
+			"dump memory (64 octets per line), pause and repeat\n");
 		return (0);
 	}
 	else
@@ -39,7 +54,7 @@ int check_s(char **av, int value, int i)
 	return (1);
 }
 
-int check_l(int value)
+int	check_l(int value)
 {
 	if (value == 1)
 		vm.log_level |= LIVE;
@@ -59,7 +74,8 @@ int check_l(int value)
 		ft_putstr("                   2  : Show cycles\n");
 		ft_putstr("                   4  : Show operations\n");
 		ft_putstr("                   8  : Show deaths\n");
-		ft_putstr("                   16 : Show PC movements (Except for jumps)\n");
+		ft_putstr("                   16 : "
+			"Show PC movements (Except for jumps)\n");
 		return (0);
 	}
 	return (1);
