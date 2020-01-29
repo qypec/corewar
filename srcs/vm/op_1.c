095a81b1295c628a981067d1e3ad1248585643c7
vgerold <eu@concreteresult.ru>
2020-01-29 17:43:46 +0300

    log verbose vlv 4 for sti, add, fork, zjmp, ld corrected

--- srcs/vm/op_1.c
+++ srcs/vm/op_1.c
@@ -31,7 +31,7 @@ void	live_op(t_process *proc)
 		vm.last_alive = &vm.players[player_index];
 	}
 	if (vm.log_level & OPERA)
-		ft_printf("P %d | live %d\n", proc->proc_id, number);
+		universal_op_log(proc, number, 0, 0);
 	if (vm.log_level & LIVE)
 		ft_printf("Player %d (%s) is said to be alive\n",
 				  proc->player_id, vm.players[proc->player_id].name);
