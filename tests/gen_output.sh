0eb840a557b21115edcf2fad46546d4882f17e95
vgerold <eu@concreteresult.ru>
2020-01-31 17:48:43 +0300

    a

--- tests/gen_output.sh
+++ tests/gen_output.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 mkdir -p 00 01
-rm -rf 00/* 01/*
+path="./champs/champs_op"
+#rm -rf 00/* 01/*
 op=live
 
 #cd standart_operations; for i in $(ls); do ../../../bin/asm $i; done
 #cd ..;
 
-for pl in $(find ./standart_operations/* -name 'live.cor')
+for pl in $(find $(echo $path)/* -name '*.cor')
 do
   pl_sum=$(../corewar_ -l 2 $pl | grep -v "ontestant" | grep -v "die" | tail -n 1 | cut -d ' ' -f 5)
   echo "player = $pl"
@@ -15,7 +16,7 @@ do
   for ((p = 0; p <= pl_sum; ++p))
   do
 #    echo "p = $p"
-    ../corewar -dump $p $pl > 00_/$(echo $pl | cut -d / -f 3 | cut -d . -f 1)_$p.out
-    ../corewar_ -dump $p $pl > 01_/$(echo $pl | cut -d / -f 3 | cut -d . -f 1)_$p.out
+    ./corewar -dump $p $pl > 00/$(echo $pl | cut -d / -f 3 | cut -d . -f 1)_$p.out
+    ./corewar_ -dump $p $pl > 01/$(echo $pl | cut -d / -f 3 | cut -d . -f 1)_$p.out
   done
 done
