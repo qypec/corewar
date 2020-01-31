c2e6bb2504a357edbed0e41e5b500c1a7d869989
ergottli <ergottli@student21.-school.ru>
2020-01-31 14:02:44 +0300

    fix xor log and cycles count

--- srcs/vm/op_2.c
+++ srcs/vm/op_2.c
@@ -47,11 +47,19 @@ void			or_op(t_process *proc)
 
 void			xor_op(t_process *proc)
 {
-	unsigned int res;
+	unsigned int    res;
+	unsigned int    first;
+    unsigned int    second;
 
-	res = get_arg_op(proc, 0) ^ get_arg_op(proc, 1);
+	first = get_arg_op(proc, 0);
+	second = get_arg_op(proc, 1);
+	res = first ^ second;
     proc->carry = (res == 0) ? 1 : 0;
 	proc->regs[proc->args_value[2] - 1] = (int)res;
 	if (vm.log_level & OPERA)
-		universal_op_log(proc, proc->args_value[0], proc->args_value[1], proc->args_value[2]);
+	{
+	    proc->args[0] = 2;
+	    proc->args[1] = 2; // Kostyl for log
+        universal_op_log(proc, (int)first, (int)second, proc->args_value[2]);
+    }
 }
