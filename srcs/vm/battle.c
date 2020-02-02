7015084a392580a2f42b62ac9f381cf3b8d2c812
ergottli <ergottli@student21.-school.ru>
2020-02-02 20:22:06 +0300

    fix and -l 4 st output. Fix problem, with args_type == 0. It was be valid.

--- srcs/vm/battle.c
+++ srcs/vm/battle.c
@@ -46,13 +46,10 @@ void	ft_exec_op(t_process *proc)
 		proc->args[0] = op_tab[proc->op - 1].args_types[0];
 	else
 		process_args_code(proc);
-	if (proc->op > 0 && proc->op < 17
-		&& parse_args_values(proc, proc->op, proc->pos, 1)
-		&& check_regs(proc, proc->op))
+	if (proc->op > 0 && proc->op < 17 && parse_args_values(proc, proc->op, proc->pos, 1))
 	{
-		if (position_correction((proc->pos + ((int) get_arg_op(proc, 1) + (int) get_arg_op(proc, 2)) % IDX_MOD)) == 73)
-			proc->pos = proc->pos;
-		op_tab[proc->op - 1].operations(proc);
+		if (check_regs(proc, proc->op))
+			op_tab[proc->op - 1].operations(proc);
 		if (vm.log_level & PC )
 			if (proc->op != 9 || (proc->op == 9 && !proc->carry))
 				print_proc_movement(proc->pos, proc->pc);
