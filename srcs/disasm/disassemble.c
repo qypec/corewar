713f1a9263fcc38a8ca8cf387e90770662eab904
qypec <qypec.prog@yandex.ru>
2020-02-03 18:38:14 +0300

    added init_asm_file

--- srcs/disasm/disassemble.c
+++ srcs/disasm/disassemble.c
@@ -6,7 +6,7 @@
 /*   By: yquaro <yquaro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/31 15:33:04 by yquaro            #+#    #+#             */
-/*   Updated: 2020/01/31 16:13:03 by yquaro           ###   ########.fr       */
+/*   Updated: 2020/02/03 18:37:38 by yquaro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,21 @@ static void				usage(void)
 
 int						main(int argc, char **argv)
 {
-	int					fd;
+	int					fd_cor;
+	int					fd_asm;
 	t_parser			*parser;
 	
 	if (argc == 1)
 		usage();
-	if ((fd = open(argv[1], O_RDONLY)) == -1)
+	if ((fd_cor = open(argv[1], O_RDONLY)) == -1)
 		exit(-1);
-	parser = init_parser(fd);
+	parser = init_parser(fd_cor);
 	parse_bytecode(parser);
-	close(fd);
+	fd_asm = init_asm_file(argv[0]);
+	fprintf(fd_asm, "%s %s\n%s %s\n\n", NAME_CMD_STRING, parser->name, \
+		COMMENT_CMD_STRING, parser->comment);
+	// convert_player_code(parser);
+	close(fd_asm);
+	close(fd_cor);
 	delete_parser(&parser);
 }
